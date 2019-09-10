f354551e1f5bdf7dfd5f99f81f58906718564d00
Krishna Kumar P,Vishal <Vishal.KrishnaKumarP@Cerner.com>
2019-09-10 11:12:42 +0530

    the word is sent twice every 5 minutes

--- sendVocab.py
+++ sendVocab.py
@@ -10,6 +10,7 @@ for i in range(1, sheet.nrows):
     newList = sheet.row_values(i)
     toSend = newList[0] + ":" + newList[1]
     notify.send(toSend)
-    time.sleep(120)#wait for sometime  (2 mins here) before you send next word with meaning
-
+    time.sleep(300)#wait for sometime  (2 mins here) before you send next word with meaning
+    notify.send(toSend) #send again for revision
+    time.sleep(300)
 #notify.send('Test 1')
